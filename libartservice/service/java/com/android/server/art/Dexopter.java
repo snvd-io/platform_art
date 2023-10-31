c29bcf26f96d5b75616fd2942737df9a125560bd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-31 14:36:38 +0200

    ask PackageManager for per-package compiler filter overrides

--- libartservice/service/java/com/android/server/art/Dexopter.java
+++ libartservice/service/java/com/android/server/art/Dexopter.java
@@ -435,6 +435,13 @@ public abstract class Dexopter<DexInfoType extends DetailedDexInfo> {
                     targetCompilerFilter, "verify", "the user requests to ignore the profile");
         }
 
+        String override = mInjector.getPackageManagerLocal()
+            .maybeOverrideCompilerFilter(targetCompilerFilter, mPkg, mParams);
+
+        if (override != null) {
+            return override;
+        }
+
         return targetCompilerFilter;
     }
 
